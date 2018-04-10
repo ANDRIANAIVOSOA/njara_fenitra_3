4378e229e4a2c6c009c2e6ca1b512cf6e8d600a7
fenitra <fenitraandrianaivosoa@gmail.com>
2018-04-10 18:08:47 +0300

    Update compter_les_mots.rb

--- compter_les_mots.rb
+++ compter_les_mots.rb
@@ -1,38 +1,35 @@
-#def jean_michel_data(corpus, dictionnaire)
-corpus = "goi"
+def jean_michel_data(corpus, dictionnaire)
 	lettre = corpus.to_s
-	tabAssoc = {}
+	tabAssoc = Hash.new(0)
 	#avadika tableau associative
-	dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
-
 	for row in 0..lettre.length-1
 		for col in row..lettre.length-1
 
 			each_word = lettre[row..col] 
-			if dictionary.include?(each_word)
-				tabAssoc[each_word] = 1
+			if dictionnaire.include?(each_word)
+				#incrementation += 1
+				tabAssoc[each_word] += 1
 			end 
 		end
 	end
 
-	#return tabAssoc
-	#prendre le valeur
-	puts tabAssoc.values
-	#detail
-	puts tabAssoc
-# end
+	return tabAssoc
+	# #prendre le valeur
+	# puts tabAssoc.values
+	# #detail
+	# puts tabAssoc
+end
 
-# def ask_for_mot
-#   print "Nombre le mot : "
-#   mot = gets.chomp
-#   return mot
-# end
+def ask_for_mot
+  print "Donner la phrase : "
+  mot = gets.chomp
+  return mot
+end
 
-# def perform
-# 	dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
-# 	mot = ask_for_mot
-# 	puts jean_michel_data(mot, dictionary)
-# end
+def perform
+	dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+	mot = ask_for_mot
+	puts jean_michel_data(mot, dictionary)
+end
 
-# perform
-
+perform
